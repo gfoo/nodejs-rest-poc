66658a698e34f4791aa44d4756a8fb6e6cd548aa
gfoo <gilles.faucherand@gmail.com>
2017-12-12 10:03:06 +0100

    step 4: body parser + cors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,26 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req,res,next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched'
-    });    
-});
-
-router.post('/', (req,res,next) => {
-    res.status(201).json({
-        message: 'Order was created'
     });
 });
 
-router.get('/:orderId', (req,res,next) => {
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'Order was created',
+        order: order
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Orders details',
         id: req.params.orderId
-    });    
+    });
 });
 
-router.delete('/:orderId', (req,res,next) => {
+router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted'
     });
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,21 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req,res,next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET request to /products'
-    });    
-});
-
-router.post('/', (req,res,next) => {
-    res.status(201).json({
-        message: 'Handling POST request to /products'
     });
 });
 
-router.get('/:productId', (req,res,next) => {
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST request to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if(id === 'special') {
+    if (id === 'special') {
         res.status(200).json({
             message: 'Handling POST request to /products',
             id: id
@@ -28,13 +33,13 @@ router.get('/:productId', (req,res,next) => {
     }
 });
 
-router.patch('/:productId', (req,res,next) => {
+router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Updated product!'
     });
 });
 
-router.delete('/:productId', (req,res,next) => {
+router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Deleted product!'
     });
