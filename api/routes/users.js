55593c97d99403b9027ef95b408ffd8092163fd7
gfoo <gilles.faucherand@gmail.com>
2018-01-10 17:39:46 +0100

    user signup

---
+++ api/routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user')
+
+// router.get('/', (req, res, next) => {
+//     User.find()
+//         .exec()
+//         .then(result => {
+//             console.log(result);
+//         });
+// });
+
+router.post('/signup', (req, res, next) => {
+    User.find({
+            email: req.body.email
+        })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(422) // or 409 conflict
+                    .json({
+                        message: 'Mail exists'
+                    });
+            } else {
+                bcrypt.hash(req.body.password, 10, function (err, hash) {
+                    if (err) {
+                        res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        });
+});
+
+router.delete('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    User.remove({
+            _id: id
+        })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'User deleted!'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 
 mongoose.connect(
@@ -38,6 +39,7 @@ app.use((error, req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
